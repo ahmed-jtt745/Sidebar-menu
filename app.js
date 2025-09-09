fa5f8f2a76aa9f42ac08ef6d30edb873e58797bf
ahmed-jtt745 <ahmedjtt745@gmail.com>
2025-09-09 20:01:44 +0500

    Added closing and opening sidebar functionality

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+//Creating dropdown functionality
 let button = document.querySelectorAll('.dropdown-button');
 button.forEach((btn) => {
     btn.addEventListener("click",(e) => {
         btn.nextElementSibling.classList.toggle('show');
         btn.classList.toggle('rotate');
+        
+        if(sidebar.classList.contains('close')){
+            sidebar.classList.toggle('close');
+            sidebarButton.classList.toggle('rotate');
+        }
     });
-})
+});
+
+//Creating sidebar collapse functionality
+let sidebarButton = document.querySelector('.toggle-button');
+let sidebar = document.querySelector('#sidebar');
+
+sidebarButton.addEventListener('click', () => {
+    sidebar.classList.toggle('close');
+    sidebarButton.classList.toggle('rotate');
+});
