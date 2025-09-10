f9d80b16bfa91cad35ece36c05cca4e6611f529c
ahmed-jtt745 <ahmedjtt745@gmail.com>
2025-09-10 12:37:01 +0500

    Fixed some responsive problems

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 let button = document.querySelectorAll('.dropdown-button');
 button.forEach((btn) => {
     btn.addEventListener("click",(e) => {
+        if(!btn.nextElementSibling.classList.contains('show'))
+            closeSubMenus();
         btn.nextElementSibling.classList.toggle('show');
         btn.classList.toggle('rotate');
         
@@ -19,14 +21,16 @@ let sidebar = document.querySelector('#sidebar');
 sidebarButton.addEventListener('click', () => {
     sidebar.classList.toggle('close');
     sidebarButton.classList.toggle('rotate');
+    closeSubMenus();
+});
+function closeSubMenus(){
     let submenus = document.querySelectorAll('.sub-menu');
     submenus.forEach((submenu) => {
-        if(submenu.classList.contains('show') && sidebar.classList.contains('close')){
+        if(submenu.classList.contains('show')){
             submenu.classList.toggle('show');
             let dropbtn = submenu.parentElement.querySelector('.dropdown-button');
             dropbtn.classList.toggle('rotate');
         }
             
     });
-
-});
+}
